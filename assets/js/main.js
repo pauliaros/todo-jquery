f914a9fffadea07e29197f69875f0d47b2c489b3
Paulina Aros <aros.pau@gmail.com>
2017-06-29 11:00:42 -0400

    se agrega lista con checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,35 @@ $(document).ready(function(){
 	$("button").click(function(e){
 		e.preventDefault();
 		var tarea = $("#tarea").val();
-		
+	
 		if(tarea== ""){
 			alert("Debes agregar una tarea");
 		}else{
-			$("#comentarios").append("<div><span class='center-align'>" + tarea + "</span><p class='center-align'></p></div>");
+			$("#comentarios").append("<p><input type='checkbox' class='check'>"+tarea+"<button class='delete'>Remove</button></p>");
 		}
 	});
+
+	function removeItem() {
+	  $(this).closest('p').remove();
+	}
+
+	function markThis() {
+	  if (this.checked) {
+	    $(this).closest('p').addClass('.delete');
+	  } else {
+	    $(this).closest('p').removeClass('.delete');
+	  }
+	}
+
+	$(document).on('click', '.check', markThis);
+
+	$(document).on('click', '.delete', removeItem);
+
+
 });
+
+$('#comentarios').on('change', 'input[type=checkbox]', function(event) {  //runs function when to do list checkbox is checked
+    var tareaNueva = $("input:checked").attr('value');  // create variable assign it to value of li that is checked
+    $('#done').append('<p>' + tareaNueva + '</p>');  //move selected li value to done list
+    $(this).parent().remove(); //remove task(checked) li from to do list
+  });
